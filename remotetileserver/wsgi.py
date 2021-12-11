61db7c0ff9dbe54535d0925ea9ea5fb67e530363
banesullivan <banesulli@gmail.com>
2021-12-11 02:30:35 -0700

    Fix wsgi

--- remotetileserver/wsgi.py
+++ remotetileserver/wsgi.py
@@ -1,10 +1,8 @@
 import logging
 
-from remotetileserver import app
+from remotetileserver import app  # noqa
 
 logging.getLogger("werkzeug").setLevel(logging.DEBUG)
 logging.getLogger("gdal").setLevel(logging.DEBUG)
 logging.getLogger("large_image").setLevel(logging.DEBUG)
 logging.getLogger("large_image_source_gdal").setLevel(logging.DEBUG)
-
-app.run()
