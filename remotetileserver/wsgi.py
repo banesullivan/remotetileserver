bf10dd7471f4a607463e18db92b92fe66a4f0bde
banesullivan <banesulli@gmail.com>
2021-12-11 02:27:22 -0700

    Fix wsgi

--- remotetileserver/wsgi.py
+++ remotetileserver/wsgi.py
@@ -2,11 +2,9 @@ import logging
 
 from remotetileserver import app
 
-if __name__ == "__main__":
+logging.getLogger("werkzeug").setLevel(logging.DEBUG)
+logging.getLogger("gdal").setLevel(logging.DEBUG)
+logging.getLogger("large_image").setLevel(logging.DEBUG)
+logging.getLogger("large_image_source_gdal").setLevel(logging.DEBUG)
 
-    logging.getLogger("werkzeug").setLevel(logging.DEBUG)
-    logging.getLogger("gdal").setLevel(logging.DEBUG)
-    logging.getLogger("large_image").setLevel(logging.DEBUG)
-    logging.getLogger("large_image_source_gdal").setLevel(logging.DEBUG)
-
-    app.run()
+app.run()
