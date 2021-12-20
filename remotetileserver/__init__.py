b6e9b7ef9e6c5f3cf19de4e807e69ed86fec6930
banesullivan <banesulli@gmail.com>
2021-12-19 19:20:10 -0700

    Improve config

--- remotetileserver/__init__.py
+++ remotetileserver/__init__.py
@@ -4,3 +4,5 @@ from localtileserver.tileserver.blueprint import cache, tileserver
 app = Flask(__name__)
 cache.init_app(app)
 app.register_blueprint(tileserver, url_prefix='/')
+app.config.JSONIFY_PRETTYPRINT_REGULAR = True
+app.config.SWAGGER_UI_DOC_EXPANSION = "list"
